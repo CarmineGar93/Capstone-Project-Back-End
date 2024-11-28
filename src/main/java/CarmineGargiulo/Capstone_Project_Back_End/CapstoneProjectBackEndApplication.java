188894f92e0eb65d4fbd551b64cd2425ff4a8ae9
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-11-28 18:46:56 +0100

    Initial commit

---
+++ src/main/java/CarmineGargiulo/Capstone_Project_Back_End/CapstoneProjectBackEndApplication.java
@@ -0,0 +1,13 @@
+package CarmineGargiulo.Capstone_Project_Back_End;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneProjectBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneProjectBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/CarmineGargiulo/Capstone_Project_Back_End/CapstoneProjectBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package CarmineGargiulo.Capstone_Project_Back_End;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneProjectBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
