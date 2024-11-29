432ce7882f7abb988b48dfd117e09db9e42cc7c8
Carmine Gargiulo <carmygargiulo45@gmail.com>
2024-11-29 15:32:36 +0100

    jwt class configured

---
+++ src/main/java/CarmineGargiulo/Capstone_Project_Back_End/tools/JWT.java
@@ -0,0 +1,37 @@
+package CarmineGargiulo.Capstone_Project_Back_End.tools;
+
+import CarmineGargiulo.Capstone_Project_Back_End.entities.User;
+import CarmineGargiulo.Capstone_Project_Back_End.exceptions.UnauthorizedException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JWT {
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public String generateToken(User user){
+        return Jwts.builder()
+                .expiration(new Date(System.currentTimeMillis()))
+                .issuedAt(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24))
+                .subject(String.valueOf(user.getUserId()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+    }
+
+    public void verifyToken(String token){
+        try {
+            Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secret.getBytes())).build().parse(token);
+        } catch (Exception e){
+            throw new UnauthorizedException("Invalid token");
+        }
+    }
+
+    public String extractUserIdFromToken(String token){
+        return Jwts.parser().verifyWith(Keys.hmacShaKeyFor(secret.getBytes())).build().parseSignedClaims(token).getPayload().getSubject();
+    }
+}
